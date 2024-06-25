b5f86408c12ddca1f24fa776720768d457167cf6
c-l-scholl <cscholl@macalester.edu>
2024-06-24 17:21:41 -0700

    created basic pitch table

--- next-react-shadcn-pg/app/users/page.tsx
+++ next-react-shadcn-pg/app/users/page.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 
-export default function Page() {
+import { Pitch, columns } from "./columns"
+import { DataTable } from "@/components/data-table"
+
+async function getUsers(): Promise<Pitch[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      pitcherName: "Camden Scholl",
+      contact: "Strike",
+      velocity: 82,
+	  pitchType: "FB"
+    },
+    // ...
+  ]
+}
+
+
+export default async function Page() {
+
+	const data = await getUsers();
+
 	return (
 		<section className="py-24">
 			<div className="container">
 				<h1 className="text-3xl font-bold">All Users</h1>
+				<DataTable columns={columns} data={data} />
 			</div>
 		</section>
 	)
