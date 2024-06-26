082597824e4bc8f9b94b8b402dce5420ab86bc1e
c-l-scholl <89877319+c-l-scholl@users.noreply.github.com>
2024-06-25 18:25:09 -0700

    working on side bar

---
+++ next-react-shadcn-pg/components/sidebar.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import UserItem from "./user-item";
+
+export default function Sidebar() {
+
+	return (
+		<div className="flex flex-col w-[300px] min-w-[300px] border-r min-h-screen p-4">
+			<div>
+				<UserItem/>
+			</div>
+			<div className="grow">Menu</div>
+			<div>Settings</div>
+		</div>
+	)
+}
