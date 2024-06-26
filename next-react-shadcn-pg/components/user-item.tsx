8909afa0b5399012798e2f5d6024b6fee63dc655
c-l-scholl <89877319+c-l-scholl@users.noreply.github.com>
2024-06-25 18:33:35 -0700

    command bar

--- next-react-shadcn-pg/components/user-item.tsx
+++ next-react-shadcn-pg/components/user-item.tsx
@@ -1,14 +1,20 @@
 "use client";
 
 export default function UserItem() {
+	
+	
 	return (
-		<div className="flex items-center justify-left gap-2 border rounded-[8px] p-2">
-			<div className="avatar rounded-full min-h-12 min-w-12 bg-emerald-500 text-white font-[700] flex items-center justify-center">
+		<div className="flex items-center justify-between gap-2 border rounded-[8px] p-2">
+			<div className="avatar rounded-full min-h-10 min-w-10 bg-emerald-500 text-white font-[700] flex items-center justify-center">
 				<p>CS</p>
 			</div>
-			<div>
-				<p className="text-[16px] font-bold">Camden Scholl</p>
-				<p className="text-[12px] text-neutral-500">cscholl@macalester.edu</p>
+			<div className="grow">
+				<p className="text-[16px] font-bold">
+					Camden Scholl
+				</p>
+				<p className="text-[12px] text-neutral-500">
+					cscholl@macalester.edu
+				</p>
 			</div>
 		</div>
 	)
