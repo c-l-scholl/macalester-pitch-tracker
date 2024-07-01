f0f8ebd02772aabdfff736e434a904fde1687934
c-l-scholl <cscholl@macalester.edu>
2024-07-01 08:51:32 -0500

    removed comments

--- next-react-shadcn-pg/firebase/clientApp.ts
+++ next-react-shadcn-pg/firebase/clientApp.ts
@@ -4,15 +4,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-
-// Your web app's Firebase configuration
-
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
 const firebaseConfig = {
 
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -30,9 +21,6 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 
 };
-
-
-// Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
