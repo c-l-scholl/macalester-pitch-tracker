546be2e42c6d30b9208619b133dd643effde46f6
c-l-scholl <cscholl@macalester.edu>
2024-07-09 12:24:09 -0500

    added nav bar to header

--- next-react-shadcn-pg/components/header.tsx
+++ next-react-shadcn-pg/components/header.tsx
@@ -1,10 +1,45 @@
 "use client";
+import Link from "next/link";
 import UserItem from "./user-item";
+import {
+	NavigationMenu,
+	NavigationMenuContent,
+	NavigationMenuIndicator,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	NavigationMenuTrigger,
+	navigationMenuTriggerStyle,
+	NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
 
 export default function Header() {
 	return (
-		<div className="flex justify-end gap-2 min-w-screen p-2 border-b">
-			<UserItem/>
+		<div className="flex flex-row justify-between gap-2 min-w-screen p-2 border-b items-center">
+			<div>
+				<NavigationMenu>
+					<NavigationMenuList>
+						<NavigationMenuItem>
+							<Link href="/" legacyBehavior passHref>
+								<NavigationMenuLink className={navigationMenuTriggerStyle()}>
+									Home
+								</NavigationMenuLink>
+							</Link>
+						</NavigationMenuItem>
+						<NavigationMenuItem>
+							{/* Add something here to block nav if not signed in */}
+							<Link href="/pitch-data" legacyBehavior passHref>
+								<NavigationMenuLink className={navigationMenuTriggerStyle()}>
+									Pitch Tracker
+								</NavigationMenuLink>
+							</Link>
+						</NavigationMenuItem>
+					</NavigationMenuList>
+				</NavigationMenu>
+			</div>
+			<div className="flex">
+				<UserItem />
+			</div>
 		</div>
 	);
 }
